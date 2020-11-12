680da83744f4191c25ee1f57b8863a22012a6ee8
omarlv86 <40244889+omarlv86@users.noreply.github.com>
2020-11-12 13:52:19 -0600

    Añadiendo componentes, servicios e interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddComponent } from './add/add.component';
+import { EditComponent } from './edit/edit.component';
+import { ViewComponent } from './view/view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddComponent,
+    EditComponent,
+    ViewComponent
   ],
   imports: [
     BrowserModule
