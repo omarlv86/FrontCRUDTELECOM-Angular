df65938db424a5f54a9024736c681c70e016f92e
omarlv86 <40244889+omarlv86@users.noreply.github.com>
2020-11-12 16:07:51 -0600

    Sistema de rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,15 @@ import { AddComponent } from './add/add.component';
 import { EditComponent } from './edit/edit.component';
 import { ViewComponent } from './view/view.component';
 
+import { RouterModule, Routes } from '@angular/router'; //importar modulo Router
+
+const appRoutes: Routes = [
+  { path: 'add', component: AddComponent },
+  { path: 'view', component: ViewComponent, pathMatch: 'full' },
+  { path: '',   redirectTo: '/view', pathMatch: 'full'},
+  { path: 'edit/:id', component: EditComponent}
+];
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +23,11 @@ import { ViewComponent } from './view/view.component';
     ViewComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes
+      //{ enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
